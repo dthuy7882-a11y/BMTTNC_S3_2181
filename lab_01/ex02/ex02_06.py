2ec0952fe9c90f224771f96bbb24544f10a52cb5
dthuy7882-a11y <dthuy7882@gmail.com>
2026-03-24 09:27:13 +0700

    nope

--- lab_01/ex02/ex02_06.py
+++
@@ -1,9 +0,0 @@
-input_str = input("Nhap X, Y: ")
-dimnesions=[int(x) for x in input_str.split(',')]
-rowNum=dimnesions[0]
-colNum=dimnesions[1]
-multilist = [[0 for col in range(colNum)] for row in range(rowNum)]
-for row in range(rowNum):
-    for col in range(colNum):
-        multilist[row][col]= row*col
-print (multilist)
--- lab_01/hello.py
+++
@@ -1,3 +0,0 @@
-print("Hello, World!")
-print("My name is Thuy")
-print("HUTECH University")
